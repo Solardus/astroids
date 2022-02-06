e7fbee44395cd6a23933f994e4ee918050868fbd
Solardus <lanyoncummings@gmail.com>
2022-02-06 19:17:15 +0000

    first commit

---
+++ Player.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+
+
+# Declare member variables here. Examples:
+var Bullet = preload("res://Bullet.tscn")
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():#start function
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):#update function
+	look_at(get_global_mouse_position())
+	if Input.is_action_pressed("Fire"):
+		var bulletinstance = Bullet.instance()
+		bulletinstance.position = position
+		bulletinstance.rotation_degrees = rotation_degrees
+		bulletinstance.apply_impulse(Vector2(),Vector2(1000,0).rotated(rotation))
+		get_tree().root.add_child(bulletinstance)
+
+func _physics_process(delta):
+	var direction = Vector2()
+	if Input.is_action_pressed("Move_Up"):
+		direction+=Vector2(0,-1)
+	if Input.is_action_pressed("Move_Down"):
+		direction+=Vector2(0,1)
+	if Input.is_action_pressed("Move_Left"):
+		direction+=Vector2(-1,0)
+	if Input.is_action_pressed("Move_Right"):
+		direction+=Vector2(1,0)
+	
+	
+	move_and_slide(direction*100)
