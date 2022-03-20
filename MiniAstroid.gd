0113e8021f83d6fb7c1f3db23c6ede4fff50c0a8
Solardus <lanyoncummings@gmail.com>
2022-03-20 19:02:48 +0000

    v4

--- MiniAstroid.gd
+++ MiniAstroid.gd
@@ -4,7 +4,7 @@ var Explosion = preload("res://Explosion.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const MiniAstroid = preload("res://MiniAstroid.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,10 +12,8 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	var player = get_tree().get_nodes_in_group("Player")[0]
-	var direction = (player.position-position).normalized()*50
-	move_and_collide(direction*delta)
+#func _process(delta):
+#	pass
 
 
 func _on_Area2D_body_entered(body):
@@ -24,7 +22,3 @@ func _on_Area2D_body_entered(body):
 		var explosion = Explosion.instance()
 		get_tree().get_root().add_child(explosion)
 		explosion.position = get_global_position()
-		var Miniastroid = MiniAstroid.instance()
-		get_tree().get_root().add_child(Miniastroid)
-		Miniastroid.position = get_global_position()
-		
