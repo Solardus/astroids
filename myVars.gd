3462d6c8d74183aa77c47456cf9f9486f3e0b598
Solardus <lanyoncummings@gmail.com>
2022-03-20 20:20:50 +0000

    v5.6

--- myVars.gd
+++ myVars.gd
@@ -4,14 +4,16 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 func getScore():
-	return "Hello"
+	return score
 
+func addScore():
+	score+=1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
