7d5296d0ff85a969faaf7e096d1db8a56828ebca
Solardus <lanyoncummings@gmail.com>
2022-05-08 10:45:49 +0100

    Changed Movement for custom movement algorithm Removed Starter Astroids
    W = Forward
    A = Left
    D = Right
    S = Nothing

--- Player.gd
+++ Player.gd
@@ -3,6 +3,16 @@ extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
+var rot_speed = 2.5
+var thrust = 500
+var max_vel = 400
+var friction = 0.65
+
+var rot = 0
+var pos = Vector2()
+var vel = Vector2()
+var acc = Vector2()
+
 var Bullet = preload("res://Bullet.tscn")
 var canfire = true
 # var b = "text"
@@ -10,12 +20,30 @@ var canfire = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():#start function
-	pass # Replace with function body.
+	var screen_size = get_viewport_rect().size
+	position = screen_size / 3
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):#update function
-	look_at(get_global_mouse_position())
+	
+	if Input.is_action_pressed("Move_Left"):
+		rot += rot_speed * delta
+	if Input.is_action_pressed("Move_Right"):
+		rot -= rot_speed * delta
+	if Input.is_action_pressed("Move_Up"):
+		acc = Vector2(thrust, 0).rotated(rot)
+	else:
+		acc = Vector2(0,0)
+		
+	acc += vel* -friction
+	vel += acc* delta
+	pos += vel * delta
+	
+	position = pos;
+	rotation = rot
+	
 	if Input.is_action_pressed("Fire") && canfire:
 		var bulletinstance = Bullet.instance()
 		bulletinstance.position = get_node("FirePoint").global_position
@@ -27,18 +55,17 @@ func _process(delta):#update function
 		canfire = true 
 
 func _physics_process(delta):
-	var direction = Vector2()
-	if Input.is_action_pressed("Move_Up"):
-		direction+=Vector2(0,-1)
-	if Input.is_action_pressed("Move_Down"):
-		direction+=Vector2(0,1)
-	if Input.is_action_pressed("Move_Left"):
-		direction+=Vector2(-1,0)
-	if Input.is_action_pressed("Move_Right"):
-		direction+=Vector2(1,0)
-	
-	
-	move_and_slide(direction*100)
+#	var direction = Vector2()
+#	if Input.is_action_pressed("Move_Up"):
+#		direction+=Vector2(0,-1)
+#	if Input.is_action_pressed("Move_Down"):
+#		direction+=Vector2(0,1)
+#	if Input.is_action_pressed("Move_Left"):
+#		direction+=Vector2(-1,0)
+#	if Input.is_action_pressed("Move_Right"):
+#		direction+=Vector2(1,0)
+#	move_and_slide(direction*100)
+		pass
 
 
 func _on_Area2D_body_entered(body):
